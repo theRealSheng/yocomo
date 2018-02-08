aa74e169fa5e8ece6c1117ce7e917665af06af6c
theRealSheng <sheng.huchai@gmail.com>
2018-02-08 15:01:44 +0100

    Add User Schema model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: String,
+  password: String,
+  role: ['BUYER', 'MERCHANT']
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', (req, res, next) => {
   res.render('signup-user');
 });
 
+router.post('/signup-user', (req, res, next) => {
+  const data = {
+    username: req.body.username,
+    password: req.body.password
+  };
+});
+
 module.exports = router;
