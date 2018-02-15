415c7a29320aa5f5a6526c41f9ceda227b09353a
theRealSheng <sheng.huchai@gmail.com>
2018-02-15 12:46:42 +0100

    Fix the event listeners for all button for reviews

--- public/javascripts/create-review.js
+++ public/javascripts/create-review.js
@@ -1,12 +1,11 @@
 
-const buttonReview = document.querySelectorAll('.review-btn');
-const formReview = document.querySelectorAll('.review-container');
+var buttonReview = document.getElementsByClassName('review-btn');
+var formReview = document.getElementsByClassName('review-container');
 
-buttonReview.forEach((btn) => {
-  btn.addEventListener('click', () => {
-
+for (let i = 0; i < buttonReview.length; i++) {
+  buttonReview[i].addEventListener('click', () => {
+    console.log(buttonReview[i]);
+    formReview[i].style.display === 'block' ? formReview[i].style.display = 'none' : formReview[i].style.display = 'block';
+    formReview[i].style.display === 'block' ? buttonReview[i].innerText = '- Do later' : buttonReview[i].innerText = '+ Add Review';
   });
-});
-
-formReview.style.display === 'block' ? formReview.style.display = 'none' : formReview.style.display = 'block';
-formReview.style.display === 'block' ? buttonReview.innerText = '- Do later' : buttonReview.innerText = '+ Add Review';
+}
