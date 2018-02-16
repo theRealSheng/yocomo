2b47279aecbf72e2dd994d486216046a1a99d9c2
theRealSheng <sheng.huchai@gmail.com>
2018-02-16 12:12:58 +0100

    Fix the auth redirect

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  if (!req.session.currentUser) {
-    return res.redirect('/auth/login');
-  }
-
   res.render('index');
 });
 
